4b176cb3a75de7640debddf78784a5de6015cf92
liubaoquan <liubaoquan0831@163.com>
2015-09-03 09:08:23 +0800

    init

--- core/src/main/java/com/github/hualuomoli/demo/mapper/IDemoMapper.java
+++ core/src/main/java/com/github/hualuomoli/demo/mapper/IDemoMapper.java
@@ -2,6 +2,7 @@ package com.github.hualuomoli.demo.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.springframework.stereotype.Repository;
 
@@ -12,12 +13,19 @@ import com.github.hualuomoli.demo.entity.Demo;
  * @author hualuomoli
  *
  */
-@Repository(value = "com.github.hualuomoli.demo.mapper.IDemoMapper")
+@Repository
 public interface IDemoMapper {
 
 	// insert
 	@Insert("insert into demo(id,email,age,sex) values(#{id},#{email},#{age},#{sex})")
 	int insert(Demo demo);
+
+	// update
+	int update(Demo demo);
+
+	// delete
+	@Delete("delete from demo t where t.id=#{id}")
+	int delete(Demo demo);
 
 	// find list by param
 	List<Demo> findList(Demo demo);
--- web/src/main/java/com/github/hualuomoli/demo/controller/DemoController.java
+++ web/src/main/java/com/github/hualuomoli/demo/controller/DemoController.java
@@ -7,14 +7,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.github.hualuomoli.core.test.entity.Demo;
+import com.github.hualuomoli.demo.entity.Demo;
 
 /**
  * test controller
  * @author hualuomoli
  *
  */
-@Controller(value = "com.github.hualuomoli.demo.controller.DemoController")
+@Controller
 @RequestMapping(value = "/demo")
 public class DemoController {
 
