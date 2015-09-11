8ecbe4cba98fb7bcc6de8fce62dce34ec9f0e4d9
liubaoquan <liubaoquan0831@163.com>
2015-09-11 14:19:05 +0800

    init

---
+++ tools-memcached/src/test/java/com/meetup/memcached/MemcachedClientTest.java
@@ -0,0 +1,44 @@
+package com.meetup.memcached;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MemcachedClientTest {
+
+	private static final String poolName = "hualuomoli";
+	private static MemcachedClient client = null;
+
+	@BeforeClass
+	public static void beforeClass() {
+		client = new MemcachedClient(poolName);
+
+		String[] servers = { "localhost:11211" };
+		Integer[] weights = { 1 };
+		SockIOPool pool = SockIOPool.getInstance(poolName);
+		pool.setServers(servers);
+		pool.setWeights(weights);
+
+		pool.setInitConn(10);
+		pool.setMinConn(5);
+		pool.setMaxConn(250);
+		pool.setMaxIdle(1000 * 60 * 60 * 6);
+
+		pool.setMaintSleep(30);
+		pool.setNagle(false);
+		pool.setSocketTO(3000);
+		pool.setSocketConnectTO(0);
+
+		pool.initialize();
+
+		client.setCompressEnable(true);
+		client.setCompressThreshold(64 * 1024);
+	}
+
+	@Test
+	public void test() {
+		client.set("foo", "This is a test String");
+		String bar = (String) client.get("foo");
+		System.out.println(bar);
+	}
+
+}
